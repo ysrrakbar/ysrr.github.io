45e52568ee6f59e85c4e0faf54104a38ef3f5233
MuhammadHadistRifannan <ipanjabar933@gmail.com>
2025-05-01 07:14:02 +0700

    success

--- produk/script.js
+++ produk/script.js
@@ -1,14 +1,20 @@
-function showForm() {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("formPopup").style.display = "block";
+function showForm(productElement) {
+    const imgSrc = productElement.querySelector("img").src; 
+    const title = productElement.querySelector("h4").textContent;
+    const price = productElement.querySelector("p").textContent;
+
+    document.getElementById("popup-img").src = imgSrc;
+    document.getElementById("popup-title").textContent = title;
+    document.getElementById("popup-price").textContent = price;
+
+    document.getElementById("popup").style.display = "block";
 }
 
-function hideForm() {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("formPopup").style.display = "none";
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
 }
 
-function submitForm() {
-    alert("Form telah dikirim!");
-    hideForm();
+function pesan(){
+    alert("Anda berhasil memesan");
+    closePopup();
 }
